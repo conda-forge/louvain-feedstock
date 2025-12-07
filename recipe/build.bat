d71f100a5f1e715ef92bb147b6683704681c37d5
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-06 18:21:25 -0800

    update build

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,5 +3,5 @@
 set IGRAPH_EXTRA_INCLUDE_PATH=%LIBRARY_INC%\igraph
 set IGRAPH_EXTRA_LIBRARY_PATH=%LIBRARY_LIB%
 
-%PYTHON% -m pip install . -vv --global-option="--external"
+%PYTHON% -m pip install . --no-deps --ignore-installed -vv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,4 @@ export IGRAPH_EXTRA_LIBRARY_PATH=${PREFIX}/lib
 echo "Extra include path: ${IGRAPH_EXTRA_INCLUDE_PATH}"
 echo "Extra library path: ${IGRAPH_EXTRA_LIBRARY_PATH}"
 
-$PYTHON -m pip install . -vv --global-option="--external"
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
