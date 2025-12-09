68b07a9e83f9cad098bda2b76a28115a3b6269b5
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-08 20:07:13 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,5 +3,8 @@
 set IGRAPH_EXTRA_INCLUDE_PATH=%LIBRARY_INC%\igraph
 set IGRAPH_EXTRA_LIBRARY_PATH=%LIBRARY_LIB%
 
+sed -i "s/self.use_pkgconfig = False/self.use_pkgconfig = True/" setup.py
+sed -i "s/self.external = False/self.external = True/" setup.py
+
 %PYTHON% -m pip install . --no-deps --ignore-installed -vv
 if errorlevel 1 exit 1
